3efe8af157255d1fffce6eb5c68112c9c6f0b082
Igor Okulist <okigan@yahoo.com>
2012-03-24 10:25:51 -0700

    Add unit test.

---
+++ WordToMarkdown.Test/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace WordToMarkdown.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string tmpFileName = Path.GetTempFileName();
+
+            string inputPath = Path.GetFullPath("HeadingTest.docx");
+
+            WordToMarkdown.Program p = new WordToMarkdown.Program(inputPath, tmpFileName);
+
+            string expectedFath = Path.GetFullPath("HeadingTest.md");
+
+            bool equal = EqualTextFiles(expectedFath, tmpFileName);
+
+            System.IO.File.Delete(tmpFileName);
+        }
+
+        private bool EqualTextFiles(string pathname1, string pathname2)
+        {
+            bool same = File.ReadLines(pathname1).SequenceEqual(File.ReadLines(pathname2));
+
+            return same;
+        }
+    }
+}
---
+++ WordToMarkdown.Test/data/HeadingTest.md
@@ -0,0 +1,14 @@
+
+Heading 1
+
+Heading 2
+
+Heading 3
+_
+__Heading 4
+_
+Heading  5
+_
+__Heading 6
+_
+
